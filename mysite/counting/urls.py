867f4109d1d31cba997063ac4cfe79b88bd49a09
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-12-26 06:11:17 +0200

    Views and user handling

--- mysite/counting/urls.py
+++ mysite/counting/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import homePageView, addView
+from .views import homePageView, addView, loginView, logoutView, loginActionView
 
 urlpatterns = [
     path('', homePageView, name='home'),
     path('add/', addView, name='add'),
+	path('login/', loginView, name='login'),
+	path('logout/', logoutView, name='logout'),
+    path('login/loginAction/', loginActionView, name='loginAction')
 ]
--- mysite/counting/views.py
+++ mysite/counting/views.py
@@ -1,21 +1,37 @@
 from django.http import HttpResponse
+#from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from .models import Account
+#from django.contrib.auth.models import User
+from .models import Account, User
 from django.db.models import Q
 import json
 
 
-
+#@login_required
 def addView(request):
 	user = request.user
 	iban = request.POST.get("iban", "").strip()
 	new = Account.objects.create(owner = user, iban = iban)
 	return redirect('/')
 
-
+#@login_required
 def homePageView(request):
-    return HttpResponse('moi')
-	#user = request.user
-	#accounts = Account.objects.filter(owner=user)
-	#return render(request, 'pages/index.html', dict(accounts=accounts))
+	accounts = Account.objects.filter(owner=request.session.get('user'))
+	return render(request, 'counting/index.html', dict(accounts=accounts))
+
+def loginView(request):
+	return render(request, 'counting/login.html')
+
+def logoutView(request):
+	request.session['user'] = None
+	return redirect('/counting/login')
+
+def loginActionView(request):
+	username = request.POST.get('username')
+	password = request.POST.get('password')
+	user = User.objects.filter(username=username, password=password).first()
+	
+	request.session['user'] = str(user.id)
+	if user:
+		return redirect('/counting')
+	return HttpResponse('moi')
