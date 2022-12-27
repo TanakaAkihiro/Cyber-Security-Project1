40e6db6084d95374729f2e9c40223addc82880b2
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-12-27 03:19:21 +0200

    Fixed db init

--- mysite/init_database.py
+++ mysite/init_database.py
@@ -1,11 +1,23 @@
-from simplebank.models import User
+import os
+os.environ.setdefault("DEFAULT_SETTINGS_MODULE", "config.settings")
+
+import django
+django.setup()
+
+from simplebank.models import User, Account
 
 def initialize_database():
-    user = User(username='bob', password='squarepants')
-    user.save()
+    user1 = User(username='bob', password='squarepants')
+    user1.save()
 
-    user = User(username='alice', password='redqueen')
-    user.save()
+    user2 = User(username='alice', password='redqueen')
+    user2.save()
+
+    account1 = Account(owner=user1, iban='345768134765')
+    account1.save()
+
+    account2 = Account(owner=user2, iban='987566013649')
+    account2.save()
 
 if __name__ == "__main__":
     initialize_database()
